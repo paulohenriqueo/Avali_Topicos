348225eb3d9b440046dd71b20fe03b78ba84a087
paulohenriqueo <paulohenrique300305@gmail.com>
2025-04-02 11:49:24 -0300

    :sparkles: feat: wallpaper

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:intl/intl.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity, // Ocupa toda a largura da tela
+        height: double.infinity, // Ocupa toda a altura da tela
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/homePage.jpg"), // Caminho da imagem
+            fit: BoxFit.cover, // Faz a imagem cobrir toda a tela
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [const SizedBox(height: 20)],
+        ),
+      ),
     );
   }
 }
